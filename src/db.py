8d9c30d2fbb70bc0425f7908ecb4f0347069f716
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-11-23 17:16:35 -0300

    fix(Init configuration): define the function create_db_and_tables on db file

--- src/db.py
+++ src/db.py
@@ -1,5 +1,6 @@
 from sqlmodel import create_engine, SQLModel
 
+
 sqlite_file_name = "database.sqlite"
 # define the path to the SQL lite
 sqlite_url = f"sqlite:///{sqlite_file_name}"
@@ -9,6 +10,8 @@ connect_args = {"check_same_thread": False}
 # create a motor to connect and operate with the database
 engine = create_engine(sqlite_url, echo=True, connect_args=connect_args)
 
+
 # makes the SQLModel search by the model children and use then to create
 # the database
-SQLModel.metadata.create_all(engine)
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
