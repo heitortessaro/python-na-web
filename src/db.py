7ec30296a43d4a0e4b5616da2c5c30fc927060a8
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-11-23 16:57:39 -0300

    feat(Init configuration): defines an db configuration file

---
+++ src/db.py
@@ -0,0 +1,14 @@
+from sqlmodel import create_engine, SQLModel
+
+sqlite_file_name = "database.sqlite"
+# define the path to the SQL lite
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+# configuration because we are running an asynchronous server
+connect_args = {"check_same_thread": False}
+# create a motor to connect and operate with the database
+engine = create_engine(sqlite_url, echo=True, connect_args=connect_args)
+
+# makes the SQLModel search by the model children and use then to create
+# the database
+SQLModel.metadata.create_all(engine)
